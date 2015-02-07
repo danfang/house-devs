6d54966d4197a994c11e387d1ed581b8810b6280
Justin <justinharjanto@hotmail.com>
2015-02-07 00:56:44 -0800

    added layouts and got internal structure working

---
+++ android/app/src/main/java/hack/com/househunt/internal/Constant.java
@@ -0,0 +1,14 @@
+package hack.com.househunt.internal;
+
+/**
+ * Constant class used to manage all the constants in the application.
+ */
+public class Constant {
+
+    private Constant() {
+
+    }
+
+
+
+}
---
+++ android/app/src/main/java/hack/com/househunt/internal/Util.java
@@ -0,0 +1,28 @@
+package hack.com.househunt.internal;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
+import hack.com.househunt.R;
+
+/**
+ * Static helper methods used to reduce redundancy in the application.
+ */
+public class Util {
+
+    private Util() {
+
+    }
+
+    public static void switchFragments(FragmentActivity a, Fragment newFrag, String tag) {
+        FragmentTransaction ft = a.getSupportFragmentManager().beginTransaction().addToBackStack(tag);
+        ft.replace(R.id.container, newFrag, tag).commit();
+        Log.d(tag, "Switched to " + tag + " fragment");
+    }
+
+}

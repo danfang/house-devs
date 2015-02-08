ff98a652fbbbce3d8b8c74af033c03e6874402b2
Deepak Nettem <deepaknettem@gmail.com>
2015-02-08 19:15:31 +0000

    working app

--- flaskserv/app/app.py
+++ flaskserv/app/app.py
@@ -2,10 +2,20 @@ import os
 from recommend import *
 from flask import Flask, request, Response, jsonify
 app = Flask(__name__)
+import json
+
+algores = {"CraigID":{"0":4745056806},"area":{"0":" (Greenwood, Seattle)"},"baths":{"0":3.0},"beds":{"0":3},"buyerseller":{"0":1.785714286},"coord":{"0":"47.714266, -122.355809"},"link":{"0":"\/see\/apa\/4745056806.html"},"longitude":{"0":-122.355809},"price":{"0":3499},"size":{"0":2500},"title":{"0":"3 bed, 3 bath, 2 parking fully furnished Seattle townhome"},"zipcode":{"0":98133}}
 
 @app.route("/")
 def hello():
         return "Hello from Python!"
+
+@app.route('/test', methods = ['GET', 'POST'])
+def test():
+	print algores
+	resp = jsonify(algores)
+	resp.status_code = 200
+	return resp
 
 @app.route('/getrec', methods = ['GET','POST'])
 def getRecommendations():
@@ -14,10 +24,11 @@ def getRecommendations():
 			print 'request received', request
 			jsonDat = request.json
 			print jsonDat
-			jsonDat1 = get_rec(jsonDat)
-			print '\n', jsonDat1
-			resp = jsonify(jsonDat1)
-			resp.status_code = 200
+			algores1 = get_rec(jsonDat)
+			print 'Response From Algo \n', algores1
+			responseString = json.dumps(algores1)
+			print responseString
+			resp = Response(responseString, status=200,  mimetype="application/json")
 			return resp
 	except:
 		print 'Invalid Request, Try Again'
